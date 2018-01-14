ea4943f7a7cd0d4b6b4d676a2dd119e0a47d20c4
Dimitrije Dolga <ddimitrije@gmail.com>
2018-01-15 00:31:26 +0100

    prvi lt

--- src/main/java/jwd/knjizara/service/impl/JpaTakmicenjaGodinaServiceImpl.java
+++ src/main/java/jwd/knjizara/service/impl/JpaTakmicenjaGodinaServiceImpl.java
@@ -1,38 +1,40 @@
 package jwd.knjizara.service.impl;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import jwd.knjizara.model.TakmicenjaGodina;
-import jwd.knjizara.repository.ManifestacijaRepository;
-import jwd.knjizara.service.ManifestacijaService;
+import jwd.knjizara.repository.TakmicenjaGodinaRepository;
+import jwd.knjizara.service.TakmicenjaGodinaService;
 //import jwd.knjizara.service.PrevoznikService;
 
 	@Service
 	@Transactional
-	public class JpaTakmicenjaGodinaServiceImpl implements TakmicenjaGodina {
+	public class JpaTakmicenjaGodinaServiceImpl implements TakmicenjaGodinaService {
 		@Autowired
-		private ManifestacijaRepository manifestacijaRepository;
+		private TakmicenjaGodinaRepository takmicenjaGodinaRepository;
 
 		@Override
 		public List<TakmicenjaGodina> findAll() {
-			return manifestacijaRepository.findAll();
+			return takmicenjaGodinaRepository.findAll();
 		}
 
 		@Override
-		public Manifestacija findOne(Long id) {
-			return manifestacijaRepository.findOne(id);
+		public TakmicenjaGodina findOne(Long id) {
+			return takmicenjaGodinaRepository.findOne(id);
 		}
 
 		@Override
-		public void save(Manifestacija manifestacija) {
-			manifestacijaRepository.save(manifestacija);
+		public void save(TakmicenjaGodina rakmicenjaGodina) {
+			takmicenjaGodinaRepository.save(rakmicenjaGodina);
 		}
 
 		@Override
 		public void remove(Long id) {
-			manifestacijaRepository.delete(id);
+			takmicenjaGodinaRepository.delete(id);
 		}
 }
